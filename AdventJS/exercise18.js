824b0f46c8a31fdd27a0029376aa6f818e8ba5bf
heraldofortuna <heraldofortuna@gmail.com>
2021-12-24 19:28:54 -0500

    Exercise 18 was completed

--- AdventJS/exercise18.js
+++ AdventJS/exercise18.js
@@ -1,25 +1,6 @@
 function fixFiles(files) {
-  const fix = [];
-
-  for (const file of files) {
-    for (const fixFile of fix) {
-      if (fixFile.includes(file)) {
-        fix.push(`${file}(1)`);
-      } else {
-        fix.push(file);
-      }
-    }
-  }
-
-  return fix;
+  return files.map((file, index) => {
+    const filtered = files.slice(0, index).filter((filter) => file === filter);
+    return filtered.length === 0 ? file : `${file}(${filtered.length})`;
+  });
 }
-
-const files = ["photo", "postcard", "photo", "photo", "video"];
-console.log(fixFiles(files)); // ['photo', 'postcard', 'photo(1)', 'photo(2)', 'video']
-
-const files2 = ["file", "file", "file", "game", "game"];
-console.log(fixFiles(files2)); // ['file', 'file(1)', 'file(2)', 'game', 'game(1)']
-
-// ojo que los elfos ya tenían archivos con (1)... ¡y pueden estar repetidos!
-const files3 = ["file", "file(1)", "icon", "icon(1)", "icon(1)"];
-console.log(fixFiles(files3)); // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
